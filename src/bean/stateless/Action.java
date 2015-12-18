5d72bed3e9444688aaa7b09f0868460fe09a84ee
Grizzlyrus <kap.samara@gmail.com>
2015-12-18 19:53:12 +0400

    first commit

---
+++ src/bean/stateless/Action.java
@@ -0,0 +1,8 @@
+package bean.stateless;
+
+/**
+ * Created by Den on 14.10.15.
+ */
+public interface Action {
+    public double sum(double firstNumber, double secondNumber);
+}
---
+++ src/bean/stateless/ActionImplMyMethod.java
@@ -0,0 +1,16 @@
+package bean.stateless;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+/**
+ * Created by Den on 14.10.15.
+ */
+@Stateless
+@Remote(Action.class)
+public class ActionImplMyMethod implements Action {
+    @Override
+    public double sum(double firstNumber, double secondNumber) {
+        return firstNumber+secondNumber;
+    }
+}
